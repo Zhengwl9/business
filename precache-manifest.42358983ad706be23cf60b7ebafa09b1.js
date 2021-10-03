18578641363ec75f834ab94dadabc416f3d23f7a
WeiliZheng <weili.z@popshop.live>
2021-10-03 21:29:53 +0800

    deploy

---
+++ precache-manifest.42358983ad706be23cf60b7ebafa09b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903c6252fc850d73e163ede268f51eb8",
+    "url": "/business/index.html"
+  },
+  {
+    "revision": "2f5db4c630049c62109f",
+    "url": "/business/static/css/2.ef5d3381.chunk.css"
+  },
+  {
+    "revision": "cba5594e03652a0b8f67",
+    "url": "/business/static/css/main.3b84624b.chunk.css"
+  },
+  {
+    "revision": "2f5db4c630049c62109f",
+    "url": "/business/static/js/2.90059f61.chunk.js"
+  },
+  {
+    "revision": "bc4aedc6b34dfdf097e8dd8dae8ff8cf",
+    "url": "/business/static/js/2.90059f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba5594e03652a0b8f67",
+    "url": "/business/static/js/main.9ca3b022.chunk.js"
+  },
+  {
+    "revision": "dcf29f7baa7d9f4df752",
+    "url": "/business/static/js/runtime-main.fcc1221b.js"
+  }
+]);
